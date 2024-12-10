04185d32413ddd793988326014960965e65d998f
pragya-celestial-systems <pragya.saxena@celestialsys.com>
2024-12-10 15:20:49 +0530

    Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,26 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      indent: ["error", 2],
+      eqeqeq: "error",
+      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+      "no-unused-vars": ["warn", { args: "none", ignoreRestSiblings: true }],
+      quotes: ["error", "single", { avoidEscape: true }],
+      "comma-dangle": ["error", "always-multiline"],
+      semi: ["error", "always"],
+      "no-var": "error",
+      "object-curly-spacing": ["error", "always"],
+      "max-len": ["warn", { code: 100 }],
+      "react/no-deprecated": "warn",
+      "react/function-component-definition": [
+        "error",
+        { namedComponents: "arrow-function" },
+      ],
+      "react/jsx-pascal-case": "error",
+      "react/jsx-key": "error",
+      "react/prop-types": ["warn", { skipUndeclared: true }],
+    },
+  },
 ];
